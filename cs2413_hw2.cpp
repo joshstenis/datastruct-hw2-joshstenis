13685c63d5a81f40144d75d34f19d8d986c4ade3
joshstenis <joshstenis@gmail.com>
2020-09-18 19:42:36 -0500

    Coded prototype for binary search, need to test the construction of the linked list and binary search. Insert or delete will use binary search

--- cs2413_hw2.cpp
+++ cs2413_hw2.cpp
@@ -33,14 +33,48 @@ class Course {
             Node *ptr_next, *ptr_prev;
         };
 
+        /**
+         * Finds middle Node of a DLL whose head and tail Nodes are given
+         * @param head The first element of the DLL
+         * @param tail The last element of the DLL
+         */
+        Node *middle(Node *head, Node *tail) {
+            Node *fast = head;
+            Node *slow = head->ptr_next;
+
+            while(head != tail) {
+                fast = fast->ptr_next;
+                if(fast != tail) {
+                    fast = fast->ptr_next;
+                    slow = slow->ptr_next;
+                }
+            } return slow;
+        }
+
     public:
 
         /**
-         * Insert an element into the doubly linked list if key is not already present
-         * @param key The value to be inserted into the DLL
+         * Binary searches through the DLL whose head Node is given
+         * @param head The first element of the DLL
+         * @param key The value that is being searched for
          */
-        void insert(int key) {
+        Node *binarySearch(Node *head, int key) {
+            Node *tail, *mid;
+            do {
+                Node *mid = middle(head, tail);
+                if(mid == NULL) return NULL;                            // Null mid catch
+                else if(mid->capacity == key) return mid;               // Mid is the desired element
+                else if(mid->capacity < key) head = mid->ptr_next;      // Mid comes BEFORE the desired element in the DLL
+                else tail = mid;                                        // Mid comes AFTER the desired element in the DLL
+            } while(tail == NULL || head != tail);
+        }
 
+        /**
+         * Output the result of the given task
+         * @param result A ptr to the Node that has been dealt with ( value equal to key )
+         */
+        void output(Node *result) {
+            //
         }
 
         int main() {
@@ -50,6 +84,7 @@ class Course {
             cin >> key;
 
             cin.ignore(1, '\n');
+            Node head;
             Node *prev;
             bool firstTime = true;
 
@@ -63,15 +98,17 @@ class Course {
 
                     tmp.ptr_prev = previous.ptr_prev;
                     previous.ptr_next = &tmp;
-                } firstTime = false;
+                } else head = tmp;
 
+                firstTime = false;
                 prev = &tmp;
             }
 
             switch(strToInt(task)) {                // Execute proper task
                 case 0:
-
-                    // Binary search
+                    
+                    Node *result = binarySearch(&head, strToInt(key));
+                    output(result);
 
                     break;
                 case 1:
