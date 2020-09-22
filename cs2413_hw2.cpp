87722a5352be94e97100109d06e05b467a0735d7
joshstenis <joshstenis@gmail.com>
2020-09-22 00:27:31 -0500

    Tested code, added "input.txt" which is sample input for binary search testing. Segmentation fault occurs during binary search, ruled out middle()

--- cs2413_hw2.cpp
+++ cs2413_hw2.cpp
@@ -9,43 +9,30 @@ using namespace std;
  * 
  */
 
+/**
+ * This struct will be each node of the DLL
+ */
+struct Node {
+    int capacity;
+    Node *ptr_next, *ptr_prev;
+
+    /**
+     * Checks if two Node objects are the identical
+     * @param n The Node object to be compared to the first Node object ( Node.compare(Node) )
+     */
+    bool equals(Node n) {
+        if(this->capacity != n.capacity) return false;
+        else if(this->ptr_next != n.ptr_next) return false;
+        else if(this->ptr_prev != n.ptr_prev) return false;
+
+        return true;
+    }
+};
+
 
 class Course {
 
-        /**
-         * This struct will be each node of the DLL
-         */
-        struct Node {
-            int capacity;
-            Node *ptr_next, *ptr_prev;
-
-            /**
-             * Checks if two Node objects are the identical
-             * @param n The Node object to be compared to the first Node object ( Node.compare(Node) )
-             */
-            bool equals(Node n) {
-                if(this->capacity != n.capacity) return false;
-                else if(this->ptr_next != n.ptr_next) return false;
-                else if(this->ptr_prev != n.ptr_prev) return false;
-
-                return true;
-            }
-        };
-
         Node *head, *tail;
-
-        /**
-         * Converts a string number into an integer -- (ex. "-140" == -140)
-         * @param str The string number
-         */
-        int strToInt(string str) {
-            int val = 0;
-            for(int i=0 ;i < str.length(); i++) {
-                if(str[i] != '-')
-                    val += pow(10, str.length()-i) * (str[i] - 48);
-            } if(str[0] == '-') val *= -1;
-            return val/10;
-        }
 
         /**
          * Finds middle Node of a DLL whose head and tail Nodes are given
@@ -55,16 +42,18 @@ class Course {
         Node *middle(Node *head, Node *tail) {
             Node *fast = head;
             Node *slow = head->ptr_next;
-            static int index = 0;
+
+            cout << "MIDDLE STARTED...";
 
             while(fast != tail) {
                 fast = fast->ptr_next;
                 if(fast != tail) {
                     fast = fast->ptr_next;
                     slow = slow->ptr_next;
-                    index++;
                 }
-            } return slow;
+            } cout << " MIDDLE COMPLETE" << endl;
+            
+            return slow;
         }
 
     public:
@@ -78,15 +67,13 @@ class Course {
          * @param new The Node to be added to the DLL
          */
         void append(Node n) {
-            Node nCopy = n;
-            
             if(this->head == NULL) {                // Empty DLL
-                this->head = &nCopy;
+                this->head = &n;
                 this->tail = this->head;
             } else {                                // Non-Empty DLL
-                nCopy.ptr_prev = this->tail;
-                this->tail->ptr_next = &nCopy;
-                this->tail = &nCopy;
+                n.ptr_prev = this->tail;
+                this->tail->ptr_next = &n;
+                this->tail = &n;
             }
         }
 
@@ -95,16 +82,22 @@ class Course {
          * @param head The first element of the DLL
          * @param key The value that is being searched for
          */
-        Node *binarySearch(Node *head, int key) {
-            Node *tail, *mid;
+        Node *binarySearch(Node *head, Node *tail, int key) {
+            Node *h = head;
+            Node *t = tail;
+            Node *mid;
+
+            cout << "THE SEARCH HAS BEGUN..." << endl;
+
             do {
-                Node *mid = middle(head, tail);
+                Node *mid = middle(h, t);
 
                 if(mid == NULL) return NULL;                            // Null mid catch
                 else if(mid->capacity == key) return mid;               // Mid is the desired element
-                else if(mid->capacity < key) head = mid->ptr_next;      // Mid comes BEFORE the desired element in the DLL
-                else tail = mid;                                        // Mid comes AFTER the desired element in the DLL
-            } while(tail == NULL || head != tail);
+                else if(mid->capacity < key) h = mid->ptr_next;      // Mid comes BEFORE the desired element in the DLL
+                else t = mid;                                        // Mid comes AFTER the desired element in the DLL
+            } while(t == NULL || h != t);
+            return NULL;
         }
 
         /**
@@ -116,6 +109,14 @@ class Course {
         }
 
         /**
+         * Returns the tail Node of the DLL
+         * @return The tail Node of the DLL
+         */
+        Node *getTail() {
+            return tail;
+        }
+
+        /**
          * Output the result of the given task
          * @param result A ptr to the Node that has been dealt with ( value equal to key )
          */
@@ -123,7 +124,7 @@ class Course {
             if(result == NULL) {
                 cout << "The given key has not been found in this list" << endl;
             } else {
-                cout << "The given key " << (*result).capacity << " has been found in this list" << endl;
+                cout << "The given key " << result->capacity << " has been found in this list" << endl;
             }
         }
 
@@ -134,47 +135,82 @@ class Course {
         void output(Course *result) {
             //
         }
-
-        int main() {
-            string task, key;
-            cin >> task;                    // Intake the task and seach key
-            cin >> key;
-
-            string val = "";
-            Course list = *(new Course());
-
-            cin.ignore(1, '\n');
-            while(val != "s") {             // Populate doubly linked list based on second line of string
-                cin >> val;
-                Node tmp;
-
-                tmp.capacity = strToInt(val);
-                list.append(tmp);
-
-
-                // if(!firstTime) {
-                    // Node previous = *prev;
-                    // tmp.ptr_prev = previous.ptr_prev;
-                    // previous.ptr_next = &tmp;
-                // } else Course list (&tmp);
-
-                // firstTime = false;
-                // prev = &tmp;
-            }
-
-            switch(strToInt(task)) {                // Execute proper task
-                case 0:
-                    
-                    Node *result = binarySearch(list.getHead(), strToInt(key));
-                    output(result);
-
-                    break;
-                case 1:
-
-                    // Insert / Delete
-
-                    break;
-                default: break;
-            }
-        }
 };
+
+/**
+ * CONSTRUCTOR for empty Course object ( DLL )
+ */
+Course::Course() {
+    head = NULL;
+    tail = NULL;
+}
+
+/**
+ * CONSTRUCTOR providing a head Node object
+ * @param head Ptr to the head Node object of the DLL
+ */
+Course::Course(Node *head) {
+    this->head = head;
+
+    Node *tmp = head;
+    while(tmp != NULL) {                        // Sets tail value to last element in DLL rabbit hole
+        if(tmp->ptr_next == NULL) break;
+        else tmp = tmp->ptr_next;
+    } tail = tmp;
+}
+
+/**
+ * Converts a string number into an integer -- (ex. "-140" == -140)
+ * @param str The string number
+ */
+int strToInt(string str) {
+    int val = 0;
+    for(int i=0 ;i < str.length(); i++) {
+        if(str[i] != '-')
+            val += pow(10, str.length()-i) * (str[i] - 48);
+    } if(str[0] == '-') val *= -1;
+    return val/10;
+}
+
+int main() {
+    string task, key;
+    cin >> task;                    // Intake the task and seach key
+    cin >> key;
+
+    string val = "";
+    Course *list = new Course();
+
+    cin.ignore(1, '\n');
+    while(val != "s") {             // Populate doubly linked list based on second line of string
+        cin >> val;
+        Node tmp;
+
+        tmp.capacity = strToInt(val);
+        list->append(tmp);
+
+
+        // if(!firstTime) {
+            // Node previous = *prev;
+            // tmp.ptr_prev = previous.ptr_prev;
+            // previous.ptr_next = &tmp;
+        // } else Course list (&tmp);
+
+        // firstTime = false;
+        // prev = &tmp;
+    } cout << "POPULATED" << endl;
+
+    switch(strToInt(task)) {                // Execute proper task
+        case 0:
+            {
+                Node *result = list->binarySearch(list->getHead(), list->getTail(), strToInt(key));
+                cout << "SEARCH COMPLETE: " << result->capacity << endl;
+                list->output(result);
+            } break;
+        case 1:
+            {} break;
+            // Insert / Delete
+        default: break;
+    }
+
+    return 0;
+}
