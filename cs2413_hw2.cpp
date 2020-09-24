8f3bfb2488745e3e7fa7ffa5eda7d8fab2cdf7da
joshstenis <joshstenis@gmail.com>
2020-09-24 13:32:37 -0500

    Binary search ACTUALLY works now. Fixed output conditional, weird thing with ptrs and double ptrs

--- cs2413_hw2.cpp
+++ cs2413_hw2.cpp
@@ -14,27 +14,52 @@ class Course {
             prev = NULL;
         }
 
+        /**
+         * Returns the capacity
+         * @return The capacity 
+         */
         int getCap() {
             return capacity;
         }
 
+        /**
+         * Sets the next ptr
+         * @param next The ptr to be set as next
+         */
         void setNext(Course *next) {
             this->next = next;
         }
 
+        /**
+         * Sets the prev ptr
+         * @param prev The ptr to be set as prev
+         */
         void setPrev(Course *prev) {
             this->prev = prev;
         }
 
+        /**
+         * Returns the next ptr
+         * @return The next ptr 
+         */
         Course *getNext() {
             return next;
         }
 
+        /**
+         * Returns the prev ptr
+         * @return The prev ptr 
+         */
         Course *getPrev() {
             return prev;
         }
 };
 
+/**
+ * Finds the tail node of the DLL containing the given node
+ * @param node Any node in the DLL ( doesnt matter which node in the DLL )
+ * @return A ptr to the tail node of the DLL
+ */
 Course *getTail(Course **node) {
     Course *iter = *node;
     while(iter->getNext() != NULL)
@@ -42,6 +67,11 @@ Course *getTail(Course **node) {
     return iter;
 }
 
+/**
+ * Finds the head node of the DLL containing the given node
+ * @param node Any node in the DLL ( doesnt matter which node in the DLL )
+ * @return A ptr to the head node of the DLL
+ */
 Course *getHead(Course **node) {
     Course *iter = *node;
     while(iter->getPrev() != NULL)
@@ -49,6 +79,11 @@ Course *getHead(Course **node) {
     return iter;
 }
 
+/**
+ * Inserts a node of given value at the end of given DLL
+ * @param head The head of given DLL
+ * @param capacity The value to be stored in the node
+ */
 void insertEnd(Course **head, int capacity) {
     Course *node = new Course(capacity);
 
@@ -60,6 +95,11 @@ void insertEnd(Course **head, int capacity) {
     }
 }
 
+/**
+ * Finds the middle node of the given DLL ( or segment thereof )
+ * @param start The starting bound for the DLL ( or segment thereof ) to be median'd
+ * @param end The ending bound for the DLL ( or segment thereof ) to be median'd
+ */
 Course *middle(Course **start, Course **end) {
     Course *fast = (*start)->getNext();
     Course *slow = *start;
@@ -77,11 +117,14 @@ Course *middle(Course **start, Course **end) {
     return slow;
 }
 
+/**
+ * Binary searches the DLL given for the key value given
+ * @param head The head node of the DLL to be searched
+ * @param key The value being searched for
+ */
 Course *binarySearch(Course **head, int key) {
     Course *h = *head;
     Course *t = getTail(head);
-
-    cout << "THE SEARCH HAS BEGUN..." << endl;
 
     do {
         Course *mid = middle(&h, &t);
@@ -94,11 +137,21 @@ Course *binarySearch(Course **head, int key) {
     return NULL;
 }
 
-void output(Course **result, int key) {
-    if(result == NULL) cout << "Value " << key << " was not found" << endl;
+/**
+ * DEBUGGING: Outputs the result of binary search
+ * @param result The resulting
+ * @param key The key value
+ */
+void outputSearchResult(Course **result, int key) {
+    if(*result == NULL) cout << "Value " << key << " was not found" << endl;
     else cout << "Value " << key << " was found." << endl;
 }
 
+/**
+ * DEBUGGING: Returns the length of the given DLL
+ * @param head The head of the DLL
+ * @return The length of the DLL
+ */
 int dllLength(Course **head) {
     Course *iter = *head;
     int i = 0;
@@ -127,7 +180,7 @@ int main() {
         case 0:
         {
             Course *result = binarySearch(&head, key);
-            output(&result, key);
+            outputSearchResult(&result, key);
         } break;
 
         case 1:
